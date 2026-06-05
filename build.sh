ae2906e08797d156589c627e459326b101e21cb7
Itamar Nascimento <itamarasa@gmail.com>
2026-06-05 16:16:54 -0300

    feat: add support for VITE_API_URL build argument and integrate CI/CD deployment workflow

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 user=$1
+url=$2
+
 commit=$(git rev-parse --short HEAD)
 
 
-docker build -t $user/salon-frontend:$commit .
+docker build --build-arg VITE_API_URL=$url -t $user/salon-frontend:$commit .
 docker tag $user/salon-frontend:$commit $user/salon-frontend:latest
 docker push $user/salon-frontend:$commit
 docker push $user/salon-frontend:latest
